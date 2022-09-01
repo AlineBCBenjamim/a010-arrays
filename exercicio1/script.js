4c9975a7a7676653ff9695d274b6ede137aa87ee
Aline Carvalho <alineoak9@gmail.com>
2022-09-01 17:49:28 -0300

    Resolução de Exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+// O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+
+const listaNumeros = [5,10,50,25,70,40,45,35,30,2];
+
+//O segundo array deve possuir apenas **strings**;
+
+const listaNomes = ["morango", "uva", "banana", "maça"];
+
+//O terceiro array deve possuir **números**, **strings** e **booleanos**.
+
+const provaDeVida = ["Maria", 70, true];
+
+console.log(listaNumeros);
+console.log(listaNomes);
+console.log(provaDeVida);
